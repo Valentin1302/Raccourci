34ec59a2621e0eb50af7ce9eb66de8e676eb03cd
Valentin Gautier <valentin.gautier@seqens.fr>
2025-04-29 14:37:15 +0200

    creation des fichiers

--- src/App.vue
+++ src/App.vue
@@ -3,35 +3,18 @@
 </script>
 
 <template>
+  <header class="bg-white shadow-md p-4 mb-4 min-h-16">
+
+    <nav>
+      <ul class="flex space-x-4">
+        <li><a href="#" class="text-blue-600 hover:underline">Home</a></li>
+        <li><a href="#" class="text-blue-600 hover:underline">About</a></li>
+        <li><a href="#" class="text-blue-600 hover:underline">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
   <h1 class="text-4xl font-bold text-blue-600 text-center mt-10">test vue js</h1>
-  
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
