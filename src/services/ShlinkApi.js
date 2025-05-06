2c7a829ff8c46b5c19bb437eb431783c61227dbd
Valentin Gautier <valentin.gautier@seqens.fr>
2025-05-06 10:20:02 +0200

    sécurtié clé API

--- src/services/ShlinkApi.js
+++ src/services/ShlinkApi.js
@@ -1,10 +1,12 @@
 import { ofetch } from 'ofetch';
 
+const SHLINK_API_KEY = import.meta.env.VITE_SHLINK_API_KEY;
+
 export const createShortUrl = async (longUrl, options = {}) => {
   return await ofetch('https://wsf-shlink.nook.sh/rest/v3/short-urls', {
     method: 'POST',
     headers: {
-      'X-Api-Key': '26236e25-3ab8-4972-b7d5-e1095f8790d0',
+      'X-Api-Key': SHLINK_API_KEY,
       'Content-Type': 'application/json'
     },
     body: {
